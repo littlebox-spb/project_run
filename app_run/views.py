2342b70267e0d344651cf29315832ea80464d875
Alexander Altukhov <sasha@littlebox.spb.ru>
2025-05-23 14:37:47 +0300

    Task 1

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
 
-# Create your views here.
+
+@api_view(['GET'])
+def company_details(request): 
+    company = setting.COMPANY_NAME
+    slogan = setting.SLOGAN
+    contacts = setting.CONTACTS
+    return Response({'company': company, 'slogan': slogan, 'contacts': contacts})
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+COMPANY_NAME = 'MyCompany'
+SLOGAN = 'MySlogan'
+CONTACTS = 'MyContacts'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
