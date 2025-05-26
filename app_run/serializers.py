022caf8854281b7da94f5671ea765b3f848dc69c
littlebox-spb <altukhov.alexandr@mail.ru>
2025-05-26 11:12:58 +0300

    Task 4.1

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,7 +4,16 @@ from rest_framework import serializers
 from .models import Run
 
 
+class RunAthleteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "last_name", "first_name"]
+
+
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = RunAthleteSerializer(source="athlete", read_only=True)
+
     class Meta:
         model = Run
         fields = "__all__"
