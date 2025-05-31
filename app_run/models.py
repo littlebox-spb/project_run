a3353bdc8779b4526b276c0a3fc6b890b9ea7f4d
Alexander Altukhov <sasha@littlebox.spb.ru>
2025-05-31 17:32:35 +0300

    Task 14.9

--- app_run/models.py
+++ app_run/models.py
@@ -35,4 +35,4 @@ class CollectibleItem(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     picture = models.URLField()
-    value = models.IntegerField(null=True)
+    value = models.IntegerField(null=True, blank=True)
